c548dc8de8069068e958b2ee6ea3dfbe7f104b43
VenkataRao <venkataraolinux4@gmail.com>
2025-09-27 16:41:48 +0530

    session 6

--- 10-condition.sh
+++ 10-condition.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
-
 NUMBER=$1
 if [$NUMBER -lt 10 ]; then
     echo "given number $NUMBER is less than 10"
+elif[$NUMBER -eq 10]; then
+    echo "given number is equal to 10" 
 else
-    echo " given number $NUMBER is greater than or equal to 10"
+    echo " given number $NUMBER is greater than  10"
 fi
 
