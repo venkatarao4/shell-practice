8792e5a7fe7458686306f8164a27c5a557838e04
VenkataRao <venkataraolinux4@gmail.com>
2025-09-27 16:59:59 +0530

    session 6

--- 11-condition.sh
+++ 11-condition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Please enter the number:"
 read NUMBER 
-if [$(($NUMBER % 2)) -eq 0]; then 
+if [ $(($NUMBER % 2)) -eq 0 ]; then 
     echo "Given number $NUMBER is EVEN"
 else
     echo "Given number $NUMBER is ODD"
